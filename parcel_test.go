55d95abadcab9700cd8784edd3825465635f5b18
Gleb Vvedensky <164244607+stxreocoma@users.noreply.github.com>
2024-07-08 20:17:35 +0300

    Add files via upload

---
+++ parcel_test.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"database/sql"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	// randSource источник псевдо случайных чисел.
+	// Для повышения уникальности в качестве seed
+	// используется текущее время в unix формате (в виде числа)
+	randSource = rand.NewSource(time.Now().UnixNano())
+	// randRange использует randSource для генерации случайных чисел
+	randRange = rand.New(randSource)
+)
+
+// getTestParcel возвращает тестовую посылку
+func getTestParcel() Parcel {
+	return Parcel{
+		Client:    1000,
+		Status:    ParcelStatusRegistered,
+		Address:   "test",
+		CreatedAt: time.Now().UTC().Format(time.RFC3339),
+	}
+}
+
+// TestAddGetDelete проверяет добавление, получение и удаление посылки
+func TestAddGetDelete(t *testing.T) {
+	// prepare
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
+
+	// add
+	id, err := store.Add(parcel)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	// get
+	parcel.Number = id
+	testParcel, err := store.Get(id)
+	require.NoError(t, err)
+	require.Equal(t, parcel, testParcel)
+
+	// delete
+	err = store.Delete(id)
+	require.NoError(t, err)
+
+	_, err = store.Get(id)
+	require.ErrorIs(t, err, sql.ErrNoRows)
+}
+
+// TestSetAddress проверяет обновление адреса
+func TestSetAddress(t *testing.T) {
+	// prepare
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
+	// add
+	id, err := store.Add(parcel)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	// set address
+	newAddress := "new test address"
+	err = store.SetAddress(id, newAddress)
+	require.NoError(t, err)
+
+	// check
+	parcel.Number = id
+	testParcel, err := store.Get(id)
+	require.NoError(t, err)
+	require.Equal(t, newAddress, testParcel.Address)
+}
+
+// TestSetStatus проверяет обновление статуса
+func TestSetStatus(t *testing.T) {
+	// prepare
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
+
+	// add
+	id, err := store.Add(parcel)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	// set status
+	err = store.SetStatus(id, ParcelStatusSent)
+	require.NoError(t, err)
+
+	// check
+	testParcel, err := store.Get(id)
+	require.NoError(t, err)
+	require.Equal(t, ParcelStatusSent, testParcel.Status)
+}
+
+// TestGetByClient проверяет получение посылок по идентификатору клиента
+func TestGetByClient(t *testing.T) {
+	// prepare
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := NewParcelStore(db)
+
+	parcels := []Parcel{
+		getTestParcel(),
+		getTestParcel(),
+		getTestParcel(),
+	}
+	parcelMap := map[int]Parcel{}
+
+	// задаём всем посылкам один и тот же идентификатор клиента
+	client := randRange.Intn(10_000_000)
+	parcels[0].Client = client
+	parcels[1].Client = client
+	parcels[2].Client = client
+
+	// add
+	for i := 0; i < len(parcels); i++ {
+		id, err := store.Add(parcels[i])
+		require.NoError(t, err)
+		require.NotEmpty(t, id)
+
+		// обновляем идентификатор добавленной у посылки
+		parcels[i].Number = id
+
+		// сохраняем добавленную посылку в структуру map, чтобы её можно было легко достать по идентификатору посылки
+		parcelMap[id] = parcels[i]
+	}
+
+	// get by client
+	storedParcels, err := store.GetByClient(client)
+	require.NoError(t, err)
+	require.Equal(t, len(parcels), len(storedParcels))
+
+	// check
+	for _, parcel := range storedParcels {
+		// в parcelMap лежат добавленные посылки, ключ - идентификатор посылки, значение - сама посылка
+		testParcel, ok := parcelMap[parcel.Number]
+		require.True(t, ok)
+		require.Equal(t, testParcel, parcel)
+	}
+}
